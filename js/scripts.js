877414f15c9771ce2f01c35134b22e8fe8df4874
Isy <isy1997@freenet.de>
2023-09-19 20:01:37 +0200

    Create variable pokemonList in scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+let pokemonList = [
+    {name: 'Psyduck', height: 79, types: ['Water'], abilities: ['Damp', 'Cloud Nine']},
+    {name: 'Clefairy', height: 61, types: ['Fairy'], abilities: ['Cute Charm', 'Magic Guard']},
+    {name: 'Slowpoke', height: 119, types: ['Water', 'Psychic'], abilities: ['Oblivious', 'Own Tempo']}
+];
+
