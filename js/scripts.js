e6e34fe322b7774cd6e49c2965013884e54d93eb
Isy <isy1997@freenet.de>
2023-09-23 16:55:06 +0200

    Add for-loop to display each pokemon's name and height

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,7 @@ let pokemonList = [
     {name: 'Clefairy', height: 61, types: ['Fairy'], abilities: ['Cute Charm', 'Magic Guard']},
     {name: 'Slowpoke', height: 119, types: ['Water', 'Psychic'], abilities: ['Oblivious', 'Own Tempo']}
 ];
-
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ' cm) ')
+}
+    
