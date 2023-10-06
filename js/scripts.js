e0104d2191a63fbb05341469925e0dc1d9b00b24
Isy <isy1997@freenet.de>
2023-10-06 14:16:13 +0200

    Change pokemon for loop to forEach() function

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,11 @@ let pokemonList = [
     {name: 'Clefairy', height: 61, types: ['Fairy'], abilities: ['Cute Charm', 'Magic Guard']},
     {name: 'Slowpoke', height: 119, types: ['Water', 'Psychic'], abilities: ['Oblivious', 'Own Tempo']}
 ];
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height >100){ //if pokemon's height is over 100cm also print 'Wow that's big
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ' cm) - Wow, that\'s big!<br>');
+pokemonList.forEach(function(pokemon) {
+    if (pokemon.height >100){ //if pokemon's height is over 100cm also print 'Wow that's big
+    document.write(pokemon.name + ' (height: ' + pokemon.height + ' cm) - Wow, that\'s big!<br>');
     }else {
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ' cm)<br>');
+    document.write(pokemon.name + ' (height: ' + pokemon.height + ' cm)<br>');
     }
-}
+});
     
