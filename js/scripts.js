af3a6e58bfc0773c0bbbdd8ec9facfdf73850c5d
Isy <isy1997@freenet.de>
2023-10-08 17:17:41 +0200

    Create addListItem function in IIFE and call it in forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -16,24 +16,29 @@ let pokemonRepository = (function () {
         }
     }
 
+    function addListItem(pokemon){
+        let pokemonList = document.querySelector('.pokemon-list');
+        // creating li element inside the ul
+        let listpokemon = document.createElement('li');
+        // creating button element inside the li
+        let button = document.createElement('button');
+        button.innerText = pokemon.name;
+        button.classList.add('button-class');
+        // Append button to the li listpokemon as its child
+        listpokemon.appendChild(button);
+        // Append the li listpokemon to the ul pokemonList as its child
+        pokemonList.appendChild(listpokemon);
+    }
+
     return {
         getAll: getAll,
-        add: add
+        add: add,
+        addListItem: addListItem
     };
 })();
 
 pokemonRepository.getAll().forEach(function(pokemon) {
-    let pokemonList = document.querySelector('.pokemon-list');
-    // creating li element inside the ul
-    let listpokemon = document.createElement('li');
-    // creating button element inside the li
-    let button = document.createElement('button');
-    button.innerText = pokemon.name;
-    button.classList.add('button-class');
-    // Append button to the li listpokemon as its child
-    listpokemon.appendChild(button);
-    // Append the li listpokemon to the ul pokemonList as its child
-    pokemonList.appendChild(listpokemon);
+    pokemonRepository.addListItem(pokemon);
 
     /*if (pokemon.height >100){ //if pokemon's height is over 100cm also print 'Wow that's big'
     document.write(pokemon.name + ' (height: ' + pokemon.height + ' cm) - Wow, that\'s big!<br>');
