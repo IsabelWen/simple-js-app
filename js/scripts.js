a7725253130ad9c9c7e12a547a0c7d6e8333458d
Isy <isy1997@freenet.de>
2023-10-15 18:16:35 +0200

    Add loadDetails() function to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,24 +5,8 @@ let pokemonRepository = (function () {
 
     // Other functions
 
-    function loadList() {
-        return fetch(apiUrl).then(function (resposne) {
-            return Response.json();
-        }).then(function (json) {
-            json.results.forEach(function (item) {
-                let pokemon = {
-                    name: item.name,
-                    detailsUrl: item.url
-                };
-                add(pokemon);
-            });
-        }).catch(function (e) {
-            console.error(e);
-        })
-    }
-
     function getAll() {
-        return repository;
+        return pokemonList;
     }
 
     function add(pokemon) {
@@ -49,14 +33,49 @@ let pokemonRepository = (function () {
         });
     }
 
+    function loadList() {
+        return fetch(apiUrl).then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            json.results.forEach(function (item) {
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                add(pokemon);
+            });
+        }).catch(function (e) {
+            console.error(e);
+        })
+    }
+
+    function loadDetails(item) {
+        let url = item.detailsUrl;
+        return fetch(url).then(function (response) {
+            return response.json();
+        }).then(function (details) {
+            // Now we add details to the item
+            item.imageUrl = details.sprites.front_default;
+            item.height = details.height;
+            item.types = details.types;
+        }).catch(function (e) {
+            console.error(e);
+        });
+    }
+
     function showDetails(pokemon){
-        console.log(pokemon) ;
+        loadDetails(pokemon).then(function () {
+            console.log(pokemon);
+        });
     }
 
     return {
         getAll: getAll,
         add: add,
-        loadList: loadList
+        loadList: loadList,
+        loadDetails: loadDetails,
+        showDetails: showDetails,
+        addListItem: addListItem
     };
 })();
 
